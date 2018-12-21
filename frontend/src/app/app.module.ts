ccc1379531ab1dbe5715e3e208ef429c7bb4f851
Wojciech Niestrój <wojciech.niestroj@billennium.com>
2018-12-21 10:55:56 +0100

    added components and routes for testing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReviewModule } from 'src/review/review.module';
+import { routing } from './testing/test.routing';
+import { Test1Component } from './testing/test1/test1.component';
+import { Test2Component } from './testing/test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
   ],
   imports: [
     BrowserModule,
     GraphQLModule,
     HttpClientModule,
-  
+    routing,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
