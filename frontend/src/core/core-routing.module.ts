f0121486ce3a8f02cbac82f4b5dea976c4636428
Mateusz Domjan <mateusz.domjan@billennium.com>
2018-12-27 10:18:03 +0100

    added login route

--- frontend/src/core/core-routing.module.ts
+++ frontend/src/core/core-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
+    { path: 'login', component: LoginComponent, /*canActivate: [LoginGuard]*/ },
   ];
 
 @NgModule({
