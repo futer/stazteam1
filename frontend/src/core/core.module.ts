a8e87fca1fcce45150bca886b6ffbcd4a78a0c47
Mateusz Domjan <mateusz.domjan@billennium.com>
2018-12-27 11:05:28 +0100

    fixed formgroup problem

--- frontend/src/core/core.module.ts
+++ frontend/src/core/core.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
   ]
 })
 export class CoreModule { }
