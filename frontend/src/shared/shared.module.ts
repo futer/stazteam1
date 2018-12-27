3c5febb23c639171376f938a0d6d9d1973a0c8b2
Mateusz Domjan <mateusz.domjan@billennium.com>
2018-12-27 09:43:22 +0100

    small fix to form input

--- frontend/src/shared/shared.module.ts
+++ frontend/src/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SubmitComponent } from './forms/submit/submit.component';
 import { InputComponent } from './forms/input/input.component';
 import { RouterBTNComponent } from './components/routerBTN/router-btn/router-btn.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { RouterBTNComponent } from './components/routerBTN/router-btn/router-btn
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
   ],
   exports: [
     SubmitComponent,
