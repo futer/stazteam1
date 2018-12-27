4bb6ac493701363edae36c31d8edcd26b0ebd86f
Mateusz Domjan <mateusz.domjan@billennium.com>
2018-12-27 11:29:34 +0100

    added button to login form

--- frontend/src/core/core.module.ts
+++ frontend/src/core/core.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class CoreModule { }
