b66cf390ce2a8dce2379f70f172345d006961572
Martyna Gosk <martyna.gosk@billennium.com>
2018-12-28 09:32:56 +0100

    created lebel component

--- frontend/src/shared/shared.module.ts
+++ frontend/src/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { SubmitBtnComponent } from './forms/submit-btn/submit-btn.component';
 import { InputComponent } from './forms/input/input.component';
 import { RouterBTNComponent } from './components/routerBTN/router-btn/router-btn.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LabelComponent } from './forms/label/label.component';
 
 @NgModule({
   declarations: [
     SubmitBtnComponent,
     InputComponent,
     RouterBTNComponent,
+    LabelComponent,
   ],
   imports: [
     CommonModule,
