33cf04d2ffe34b64398934b6c7449020863fa017
Kamil Wiszniewski <kamil.wiszniewski@billennium.com>
2019-01-10 09:16:36 +0100

    fix jwt

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(jwt());
+app.use(jwt());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
