f5bb26dc67629457ad24258a2a59f01337d641ac
Kamil Wiszniewski <kamil.wiszniewski@billennium.com>
2019-01-28 14:17:46 +0100

    chat backend + frontend

--- frontend/src/app/models/registered.enum.ts
+++ frontend/src/app/models/registered.enum.ts
@@ -1,16 +1,7 @@
 export enum RegisteredEnum {
-<<<<<<< HEAD
   LOCAL = 'LOCAL',
   FACEBOOK = 'FACEBOOK',
   GOOGLE = 'GOOGLE',
   LOCAL_FACEBOOK = 'LOCAL_FACEBOOK',
   LOCAL_GOOGLE = 'LOCAL_GOOGLE',
 }
-=======
-    LOCAL = 'LOCAL',
-    FACEBOOK = 'FACEBOOK',
-    GOOGLE = 'GOOGLE',
-    LOCAL_FACEBOOK = 'LOCAL_FACEBOOK',
-    LOCAL_GOOGLE = 'LOCAL_GOOGLE',
-  }
->>>>>>> origin/user-store
--- frontend/src/app/models/role.enum.ts
+++ frontend/src/app/models/role.enum.ts
@@ -1,15 +1,7 @@
 export enum RoleEnum {
-<<<<<<< HEAD
   ADMIN = 'admin',
   REVIEWER = 'reviewer',
   EDITOR = 'editor',
   MODERATOR = 'moderator',
   USER = 'user',
-=======
-    USER = 'user',
-    EDITOR = 'editor',
-    MODERATOR = 'moderator',
-    REVIEWER = 'reviewer',
-    ADMIN = 'admin'
->>>>>>> origin/user-store
 }
