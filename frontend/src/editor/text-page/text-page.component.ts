6cdde0bb56fa11fd903a131742c65b2dd334f522
Mateusz Domjan <mateusz.domjan@billennium.com>
2019-02-08 13:57:51 +0100

    why me

--- frontend/src/editor/text-page/text-page.component.ts
+++ frontend/src/editor/text-page/text-page.component.ts
@@ -13,19 +13,34 @@ import {
     styleUrls: ['./text-page.component.scss']
 })
 export class TextPageComponent implements OnInit {
-    height = 1000;
+    height = 250; flag = false;
     @ViewChild('page') page: ElementRef;
     @HostListener('document:keydown', ['$event']) onkeydownHandler(
         event: KeyboardEvent
     ) {
-        console.log(this.page.nativeElement.clientHeight, this.page.nativeElement.scrollHeight);
+        // console.log(this.page.nativeElement.clientHeight, this.page.nativeElement.scrollHeight);
         if (this.page.nativeElement.clientHeight < this.page.nativeElement.scrollHeight) {
             console.log(this.page.nativeElement);
             this.renderer.setStyle(
                 this.page.nativeElement,
                 'height',
-                (this.height = this.height + 1000) + 'px'
+                (this.height = this.height + 250) + 'vh'
             );
+        }
+        if (this.page.nativeElement.clientWidth < this.page.nativeElement.scrollWidth && !this.flag) {
+            // let textNode = this.page.nativeElement.firstChild;
+            // let newTextNode = textNode.splitText(textNode.data.length - 2);
+            this.flag = true;
+            const br = this.renderer.createElement('br');
+            const br2 = this.renderer.createElement('br');
+            console.log(br);
+            // br.appendChild(newTextNode);
+            this.page.nativeElement.appendChild(br);
+            this.page.nativeElement.appendChild(br2);
+
+            const selection = window.getSelection();
+            selection.collapse(this.page.nativeElement.childNodes[this.page.nativeElement.childNodes.length - 1], 0);
+            console.log('wololo');
         }
     }
 
