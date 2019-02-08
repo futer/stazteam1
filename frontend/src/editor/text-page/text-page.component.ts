278bb586963efb2218dfcb6cbeb9d9da05a52019
Mateusz Domjan <mateusz.domjan@billennium.com>
2019-02-08 15:06:22 +0100

    even tho it works, my happiness is forever gone :(

--- frontend/src/editor/text-page/text-page.component.ts
+++ frontend/src/editor/text-page/text-page.component.ts
@@ -13,34 +13,29 @@ import {
     styleUrls: ['./text-page.component.scss']
 })
 export class TextPageComponent implements OnInit {
-    height = 250; flag = false;
+    height = 250;
     @ViewChild('page') page: ElementRef;
     @HostListener('document:keydown', ['$event']) onkeydownHandler(
         event: KeyboardEvent
     ) {
-        // console.log(this.page.nativeElement.clientHeight, this.page.nativeElement.scrollHeight);
         if (this.page.nativeElement.clientHeight < this.page.nativeElement.scrollHeight) {
-            console.log(this.page.nativeElement);
             this.renderer.setStyle(
                 this.page.nativeElement,
                 'height',
                 (this.height = this.height + 250) + 'vh'
             );
         }
-        if (this.page.nativeElement.clientWidth < this.page.nativeElement.scrollWidth && !this.flag) {
-            // let textNode = this.page.nativeElement.firstChild;
-            // let newTextNode = textNode.splitText(textNode.data.length - 2);
-            this.flag = true;
+        if (this.page.nativeElement.clientWidth < this.page.nativeElement.scrollWidth) {
+            const textNode = this.page.nativeElement.childNodes[this.page.nativeElement.childNodes.length - 2];
+            const newTextNode = textNode.splitText(textNode.data.length - 1);
+
             const br = this.renderer.createElement('br');
-            const br2 = this.renderer.createElement('br');
-            console.log(br);
-            // br.appendChild(newTextNode);
+
+            this.page.nativeElement.appendChild(newTextNode);
             this.page.nativeElement.appendChild(br);
-            this.page.nativeElement.appendChild(br2);
 
             const selection = window.getSelection();
-            selection.collapse(this.page.nativeElement.childNodes[this.page.nativeElement.childNodes.length - 1], 0);
-            console.log('wololo');
+            selection.collapse(this.page.nativeElement.childNodes[this.page.nativeElement.childNodes.length - 2], 1);
         }
     }
 
