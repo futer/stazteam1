665a6f30cc4ffa82c6f96f90aca6aeb78e9be251
Szw4gier <45814286+Szw4gier@users.noreply.github.com>
2019-02-12 21:12:59 +0100

    got data needed to send to db, needs service

--- frontend/src/editor/edit-controls/send-btn/send-btn.component.ts
+++ frontend/src/editor/edit-controls/send-btn/send-btn.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { PdfGeneratorService } from 'src/editor/services/pdf-generator.service';
+import { ToolboxActionsService } from 'src/editor/services/toolbox-actions.service';
+import { Store } from '@ngrx/store';
+import { zip } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-send-btn',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendBtnComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pdfGenerator: PdfGeneratorService,
+    private textRef: ToolboxActionsService,
+    private store: Store<any>
+  ) { }
 
   ngOnInit() {
   }
 
+  sendPDF() {
+    zip(
+      this.textRef.textSource,
+      this.store.pipe(map(data => data.auth.user))
+    ).subscribe(res => {
+      const doc = this.pdfGenerator.generatePDF(res[0]);
+      doc.output('datauri');
+      // TODO: Service to send data
+    }).unsubscribe();
+  }
 }
