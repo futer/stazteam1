b2f813a70e4fe5f96750c32b12b4b89e160710eb
mgosk96 <m.gosk96@gmail.com>
2019-02-14 08:54:39 +0100

    css styles missing

---
+++ frontend/src/core/error-page/error-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-error-page',
+  templateUrl: './error-page.component.html',
+  styleUrls: ['./error-page.component.scss']
+})
+export class ErrorPageComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  navigateToMain() {
+    this.router.navigate(['/main']);
+  }
+}
