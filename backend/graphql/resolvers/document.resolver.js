ce68d7aa47fe3191cc02e99297d23e31f6c0a09b
mgosk96 <m.gosk96@gmail.com>
2019-02-14 13:46:55 +0100

    Adding documents can editor and admin only

--- backend/graphql/resolvers/document.resolver.js
+++ backend/graphql/resolvers/document.resolver.js
@@ -1,4 +1,5 @@
 const documentService = require('../../services/document.service');
+const userService = require("../../services/user.service");
 
 function getDocuments(root, args, context) {
   return documentService.getDocuments(args);
@@ -8,12 +9,20 @@ function getDocument(root, args, context) {
   return documentService.getDocument(args);
 }
 
-function addDocument(root, args, context) {
-  const document = documentService.addDocument(args);
+async function addDocument(root, args, context) {
+  let document;
+  let token = context.headers.authorization.slice(7);
+
+  await userService.isEditor(token)
+    .then(() => {
+      document = documentService.addDocument(args);
+    }).catch(err => {throw err});
+  
   return document;
 }
 
 function updateDocument(root, args, context) {
+  let document;
   const document = documentService.updateDocument(args);
 
   return document;
