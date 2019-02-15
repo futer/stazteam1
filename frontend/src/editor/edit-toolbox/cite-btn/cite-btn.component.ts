6049eb83b4e7e9acdb340803340c70a44b75f0e4
Mateusz Domjan <mateusz.domjan@billennium.com>
2019-02-15 13:32:52 +0100

    working cite

--- frontend/src/editor/edit-toolbox/cite-btn/cite-btn.component.ts
+++ frontend/src/editor/edit-toolbox/cite-btn/cite-btn.component.ts
@@ -2,24 +2,29 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ToolboxActionsService } from 'src/editor/services/toolbox-actions.service';
 
 @Component({
-  selector: 'app-cite-btn',
-  templateUrl: './cite-btn.component.html',
-  styleUrls: ['./cite-btn.component.scss']
+    selector: 'app-cite-btn',
+    templateUrl: './cite-btn.component.html',
+    styleUrls: ['./cite-btn.component.scss']
 })
 export class CiteBtnComponent implements OnInit {
+    constructor(
+        private rendered: Renderer2,
+        private textRef: ToolboxActionsService
+    ) {}
 
-  constructor(
-    private rendered: Renderer2,
-    private textRef: ToolboxActionsService
-  ) { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    toggleQuoteMarks() {
+        const sel = window.getSelection().getRangeAt(0);
+        sel.startContainer['data'] =
+            sel.startContainer['data'].slice(0, sel.startOffset) +
+            '"' +
+            sel.startContainer['data'].slice(sel.startOffset);
+        sel.endContainer['data'] =
+            sel.endContainer['data'].slice(0, sel.endOffset) +
+            '"' +
+            sel.endContainer['data'].slice(sel.endOffset);
 
-  toggleQuoteMarks() {
-    this.textRef.textSource.subscribe(ref => {
-      // TODO
-      console.log(ref.nativeElement);
-    }).unsubscribe();
-  }
+        document.getSelection().empty();
+    }
 }
